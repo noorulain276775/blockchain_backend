1044c6a4a3c5c0485d4eb0e6cb7d12ac47524fa1
Noor ul ain Ibrahim <noorulainibrahim75@gmail.com>
2023-03-19 18:36:31 +0400

    wrote smart contract and deployed it in the blockchain

--- eth-to-do-list/contracts/Todolist.sol
+++ eth-to-do-list/contracts/Todolist.sol
@@ -3,5 +3,21 @@ pragma solidity^0.5.0;
 
 contract Todolist {
     uint public taskCount = 0;
+    struct Task {
+        uint id;
+        string content;
+        bool completed; 
+    }
+
+    mapping(uint => Task) public task;
+
+    constructor() public {
+        createTask("Complete the course of blockchain");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount  ++;
+        task[taskCount] = Task(taskCount, _content, false);
+    }
 
 }
